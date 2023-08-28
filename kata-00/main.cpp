f16f3b1100daf391d4bd64e6dcdc13af9f9ad281
Oprișor Adrian-Ilie <88934968+oprisor-adrian@users.noreply.github.com>
2023-08-28 20:19:55 +0300

    refactoring: moveZeros() function

--- kata-00/main.cpp
+++ kata-00/main.cpp
@@ -4,63 +4,62 @@
 #include <iostream>
 #include <string>
 #include <typeinfo>
-#include <vector>
 
-void printAnyType(const std::any& value) {
+void printElement(const std::any& element) {
   try {
-    if (value.type() == typeid(bool)) {
-      std::cout << std::any_cast<bool>(value) << ' ';
+    if (element.type() == typeid(bool)) {
+      std::cout << std::any_cast<bool>(element) << ' ';
       return;
     }
-    if (value.type() == typeid(int)) {
-      std::cout << std::any_cast<int>(value) << ' ';
+    if (element.type() == typeid(int)) {
+      std::cout << std::any_cast<int>(element) << ' ';
       return;
     }
-    if (value.type() == typeid(std::string)) {
-      std::cout << std::any_cast<std::string>(value) << ' ';
-      return;
+    if (element.type() == typeid(std::string)) {
+      std::cout << std::any_cast<std::string>(element) << ' ';
     }
   } catch (const std::bad_any_cast& e) {
-    std::cout << e.what() << std::endl;
+    std::cout << "printElement: " << e.what() << std::endl;
   }
 }
 
-void moveZeroToEnd(std::vector<std::any>& array, const unsigned int pos) {
-  for (unsigned int index=pos; index < array.size()-1; index++) {
-    std::swap(array[index], array[index+1]);
-  }
-}
-
-void moveZeros(std::vector<std::any>& array) {
+std::vector<std::any> moveZeros(const std::vector<std::any>& array) {
   try {
-    // Move each '0' element at the end of the array
-    for (unsigned int index = 0; index < array.size(); index++) {
-      std::any elem = array[index];
+    unsigned int zeros_counter = 0;
+    std::vector<std::any> temp_array;
+    for (const auto elem : array) {
       if (elem.type() == typeid(int)) {
         int temp = std::any_cast<int>(elem);
-	if (temp == 0) {
-          moveZeroToEnd(array, index);
+	if (temp == 0 ) {
+          zeros_counter++;
+	  continue;
 	}
       }
+      temp_array.push_back(elem);
     }
+    for (unsigned int index=0; index<zeros_counter; index++) {
+      temp_array.push_back(0);
+    }
+    return temp_array;
   } catch (const std::bad_any_cast& e) {
-    std::cout << e.what() << std::endl;
+    std::cout << "moveZeros: " << e.what() << std::endl;
+    return std::vector<std::any>();
   }
 }
 
 int main() {
-  std::vector<std::any> array = {0, true, 0, std::string("test"), 0, std::string("me"), 0, 5};
+  std::vector<std::any> array = {0, true, 0, std::string("text"), 0, 5};
   std::cout << std::boolalpha;
-  // Prints array before processing
-  for (std::any temp : array) {
-    printAnyType(temp);
+  // Prints arddray before processing
+  for (const auto temp : array) {
+    printElement(temp);
   }
   std::cout << std::endl;
   // Moves all the '0' to the end
-  moveZeros(array);
+  array = moveZeros(array);
   // Prints array after processing
-  for (std::any temp : array) {
-    printAnyType(temp);
+  for (const auto temp : array) {
+    printElement(temp);
   }
   std::cout << std::endl;
   return 0;
